dbc15d55a4feb73e88d13bc558e405e5b16532af
山﨑 孝二 <kouzi619soccer@icloud.com>
2024-12-15 11:22:56 +0900

    feat: プロフィールページの内容追加

--- src/components/GlobalNavigation/index.tsx
+++ src/components/GlobalNavigation/index.tsx
@@ -13,15 +13,30 @@ export const GlobalNavigation = () => {
 
   return (
     <nav>
-      <ul className="absolute left-2/4 bottom-20 -translate-x-1/2 w-80 flex justify-center items-center gap-10 px-4 py-2 border border-solid border-slate-200 rounded-full">
-        <li className={`${isAbout && selectedStyle} ease-in duration-200 hover:text-white hover:bg-blue-200 px-2 rounded-md`}>
-          <Link href="/about">知る</Link>
+      <ul className="fixed left-2/4 bottom-20 -translate-x-1/2 w-80 flex justify-center items-center gap-10 px-4 py-2 bg-white border border-solid border-slate-200 rounded-full">
+        <li className={`${isAbout && selectedStyle} ease-in duration-200 hover:text-white hover:bg-blue-200 rounded-md`}>
+          <Link
+            href="/about"
+            className="px-2"
+          >
+            知る
+          </Link>
         </li>
-        <li className={`${isRead && selectedStyle} ease-in duration-200 hover:text-white hover:bg-blue-200 px-2 rounded-md`}>
-          <Link href="/read">読む</Link>
+        <li className={`${isRead && selectedStyle} ease-in duration-200 hover:text-white hover:bg-blue-200 rounded-md`}>
+          <Link
+            href="/read"
+            className="px-2"
+          >
+            読む
+          </Link>
         </li>
-        <li className={`${isGame && selectedStyle} ease-in duration-200 hover:text-white hover:bg-blue-200 px-2 rounded-md`}>
-          <Link href="/game">遊ぶ</Link>
+        <li className={`${isGame && selectedStyle} ease-in duration-200 hover:text-white hover:bg-blue-200 rounded-md`}>
+          <Link
+            href="/game"
+            className="px-2"
+          >
+            遊ぶ
+          </Link>
         </li>
       </ul>
     </nav>
